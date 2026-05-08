700973caa8cdde25fa7bc2a3f8cfea8ff9004a80
justpeterjp1 <oluwagbotemiojopeter10@gmail.com>
2026-05-08 14:40:31 +0100

    added header component

---
+++ src/app/App.jsx
@@ -0,0 +1,20 @@
+import Header from '../components/layout/Header'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="grid lg:grid-cols-2 gap-8">
+        
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,16 @@
+import Logo from '../../assets/hero.png'
+const Header = () => {
+  return (
+       <header className="bg-white border-b border-gray-600 shadow-sm min-h-screen">
+            <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                <div className='flex items-center gap-1'>
+                <img src={Logo} alt="Logo" width="50" height="100" />
+                <h1 className="text-yellow-400 font-bold text-3xl">Sun<strong className="text-red-600">Sage</strong></h1>
+                </div>
+            <p className="text-stone-600 text-xl">Nobody understands your energy needs better than you do.</p>
+            </div>   
+        </header>
+  )
+}
+
+export default Header
